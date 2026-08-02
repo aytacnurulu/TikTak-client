73696b2e4e4c28aaa19d975c54a2d4a41edf32ff
Narminkerimova <narmingk-bf206@code.edu.az>
2026-08-03 01:09:32 +0400

    chore: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,20 @@ import "@/shared/lib/apiClient.init";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 import Providers from "./providers";
+import { Metadata } from "next";
 
 const roboto = Roboto({
   subsets: ["latin"],
   weight: ["100", "300", "400", "500", "700", "900"],
   variable: "--font-roboto",
 });
+
+export const metadata: Metadata = {
+  title: "TikTak",
+  icons: {
+    icon: "/icons/favicon.svg",
+  },
+};
 
 export default function RootLayout({
   children,
