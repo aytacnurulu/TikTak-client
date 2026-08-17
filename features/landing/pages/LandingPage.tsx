e9557602f9e9a1b8bf0a9afae79688aefd3f221c
Babek Ceferov <ceferovbabek07@gmail.com>
2026-08-18 02:47:34 +0400

    LandingPage chaing on usclient to server side

--- features/landing/pages/LandingPage.tsx
+++ features/landing/pages/LandingPage.tsx
@@ -1,26 +1,15 @@
-"use client";
-
-import Spinner from "@/shared/components/Spinner";
 import HeroCarousel from "../components/HeroCarousel/HeroCarousel";
 import SpecialOffers from "../components/SpecialOffers/SpecialOffers";
 import StatsSection from "../components/StatsSection/StatsSection";
-import { useCampaignsQuery } from "../hooks/useLanding";
+import { getCampaigns } from "@/shared/lib/api/campaigns";
 
-const LandingPage = () => {
-  const { data: campaigns, isLoading } = useCampaignsQuery();
-
-  if (isLoading) {
-    return (
-      <div className="flex justify-center py-20">
-        <Spinner size="lg" color="primary" />
-      </div>
-    );
-  }
+const LandingPage = async () => {
+  const campaigns = await getCampaigns();
 
   return (
     <div className="py-8 space-y-10">
-      <HeroCarousel campaigns={campaigns ?? []} />
-      <SpecialOffers campaigns={(campaigns ?? []).slice(2, 4)} />
+      <HeroCarousel campaigns={campaigns} />
+      <SpecialOffers campaigns={campaigns.slice(2, 4)} />
       <StatsSection />
     </div>
   );
