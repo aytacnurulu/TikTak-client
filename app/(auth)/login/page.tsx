39934d222c9b6c89f6df7984918624d04a5e4ee0
Narminkerimova <narmingk-bf206@code.edu.az>
2026-08-28 02:06:19 +0400

    refactor(auth): adapt auth pages to use loading state

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,17 +1,7 @@
-import { Suspense } from "react";
 import LoginPage from "@/features/auth/pages/LoginPage";
-import Spinner from "@/shared/components/Spinner";
 
 export default function Page() {
   return (
-    <Suspense
-      fallback={
-        <div className="flex justify-center items-center min-h-screen">
-          <Spinner size="lg" color="primary" />
-        </div>
-      }
-    >
-      <LoginPage />
-    </Suspense>
+    <LoginPage />
   );
 }
